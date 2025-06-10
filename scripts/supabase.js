671659b6f2c9c457a025853285d31573fc9a5aea
Blackhearts <fastroad09@gmail.com>
2025-06-10 15:48:59 +1300

    Update supabase.js

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,6 +1,6 @@
 // Initialize Supabase client
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://ctzvvqthrnnujtkpezpv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0enZ2cXRocm5udWp0a3BlenB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1MTczNzUsImV4cCI6MjA2NTA5MzM3NX0.Ckh7WiVUx7SYzycyLJUf98wkKqR70PwLCTFUoTYC920';
 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
